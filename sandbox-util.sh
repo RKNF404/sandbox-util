c26670b078373819deea48615c96786546ae55b7
Root <175176948+RKNF404@users.noreply.github.com>
2025-06-07 12:29:46 -0400

    chore: add license text

--- sandbox-util.sh
+++ sandbox-util.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 # Make a few environment variables immediately immutable
 declare -r HOME="$HOME"
 declare -r XDG_RUNTIME_DIR="$XDG_RUNTIME_DIR"
